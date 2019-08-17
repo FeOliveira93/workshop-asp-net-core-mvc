e49596d617429e34106afcb1c7a29f05f5390496
Luiz Felipe <lfelipe.oliveira@hotmail.com>
2019-08-17 14:12:46 -0300

    App locale, number and data format.

--- SalesWebMCV/Startup.cs
+++ SalesWebMCV/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.EntityFrameworkCore;
 using SalesWebMCV.Models;
 using SalesWebMCV.Data;
 using SalesWebMCV.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace SalesWebMCV
 {
@@ -51,6 +53,17 @@ namespace SalesWebMCV
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+
+            var enUs = new CultureInfo("en-Us");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUs),
+                SupportedCultures = new List<CultureInfo> { enUs },
+                SupportedUICultures = new List<CultureInfo> { enUs }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
